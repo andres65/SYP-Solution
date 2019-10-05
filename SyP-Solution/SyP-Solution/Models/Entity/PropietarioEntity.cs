3324792095ab794d0cab79061c1b8dcf84c64caa
Camila Camacho <mccamacho79@misena.edu.co>
2019-10-05 10:52:26 -0500

    Entity
    Entidad Propietario

---
+++ SyP-Solution/SyP-Solution/Models/Entity/PropietarioEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SyP_Solution.Models.Entity
+{
+    public class PropietarioEntity
+    {
+        public int intId { get; set; }
+        public int intNumeeroIdentificacion { get; set; }
+        public string  strNombre { get; set; }
+        public string  strApellido { get; set; }
+        public string  strDireccion { get; set; }
+        public string  strTelefono { get; set; }
+        public string  strCorreo { get; set; }
+    }
+}
